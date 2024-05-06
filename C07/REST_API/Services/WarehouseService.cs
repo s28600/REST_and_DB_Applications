e40f783807bb377f769f6bcc93fabbc694d3fc0c
s28600 <s28600@pjwstk.edu.pl>
2024-05-06 11:30:54 +0200

    Some functionality implemented

--- C07/REST_API/Services/WarehouseService.cs
+++ C07/REST_API/Services/WarehouseService.cs
@@ -1,3 +1,4 @@
+using System.Data.SqlClient;
 using REST_API.Models;
 
 namespace REST_API.Services;
@@ -8,6 +9,26 @@ public class WarehouseService(IConfiguration configuration) : IWarehouseService
     
     public void UpdateWarehouse(Warehouse warehouse)
     {
-        throw new NotImplementedException();
+        using var connection = new SqlConnection(_configuration["ConnectionStrings:DefaultConnection"]);
+        if (!ProductExists(connection, warehouse.IdProduct))
+            throw new Exception("Product with provided id does not exist");
+        if (!WarehouseExists(connection, warehouse.IdWarehouse))
+            throw new Exception("Warehouse with provided id does not exist");
+        if (warehouse.Amount <= 0 )
+            throw new Exception("Amount should be greater than 0");
+    }
+    
+    private bool ProductExists(SqlConnection connection, int id)
+    {
+        using var command = new SqlCommand("SELECT COUNT(1) FROM Product WHERE IdProduct = @Id", connection);
+        command.Parameters.AddWithValue("@id", id);
+        return (int)command.ExecuteScalar() > 0;
+    }
+
+    private bool WarehouseExists(SqlConnection connection, int id)
+    {
+        using var command = new SqlCommand("SELECT COUNT(1) FROM Warehouse WHERE IdWarehouse = @Id", connection);
+        command.Parameters.AddWithValue("@id", id);
+        return (int)command.ExecuteScalar() > 0;
     }
 }
