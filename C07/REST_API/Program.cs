b5223582f1db154e9c5eae5b4e3518848f13c3da
s28600 <s28600@pjwstk.edu.pl>
2024-05-06 11:17:51 +0200

    Fixed Program.cs

--- C07/REST_API/Program.cs
+++ C07/REST_API/Program.cs
@@ -1,3 +1,5 @@
+using REST_API.Services;
+
 namespace REST_API;
 
 public class Program
@@ -12,6 +14,8 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.Services.AddControllers();
+        builder.Services.AddScoped<IWarehouseService, WarehouseService>();
 
         var app = builder.Build();
 
@@ -21,6 +25,9 @@ public class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+        
+        app.UseHttpsRedirection();
+        app.MapControllers();
 
         app.Run();
     }
